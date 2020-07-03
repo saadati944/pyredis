5b9848059e911d1eeda881890ac7fc11057d171e
saadati944 <saadati944@gmail.com>
2020-07-03 16:29:05 +0430

    add set route

--- pyredisflask.py
+++ pyredisflask.py
@@ -7,4 +7,9 @@ app=Flask(__name__)
 def home():
     return 'you are using <b>pyredis v0.0</b><br>todo:add other contents like documentation ...'
 
+
+@app.route('/set',methods=['GET','POST'])
+def set():
+    return str(redis.set(request.values['key'],request.values['value']))
+
 app.run()
