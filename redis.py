45b8bb3de5057646f72850b9548d3b86c57d7bdf
saadati944 <saadati944@gmail.com>
2020-07-03 16:06:23 +0430

    add set function

--- redis.py
+++ redis.py
@@ -1,9 +1,29 @@
 keys=[]
 values=[]
 
+def set(key,value):
+    global keys,values
+    try:
+        i=keys.index(key)
+        values[i]=value
+        return 1
+    except:
+        try:
+            keys.append(key)
+            values.append(value)
+            return 1
+        except:
+            return 0
+    return 0
 
 def main():
     print('pyredis v0.0')
+    set('k','v')
+    print(keys,values)
+    set('k2','v2')
+    print(keys,values)
+    set('k','v3')
+    print(keys,values)
 
 if __name__=='__main__':
     main()
